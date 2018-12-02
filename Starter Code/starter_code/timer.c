87268b5859331634cb02e3d1402b3be2cc41616d
马兵 <40521247+GeassAthena@users.noreply.github.com>
2018-12-02 23:49:19 +0800

    Add files via upload

--- Starter Code/starter_code/timer.c
+++ Starter Code/starter_code/timer.c
@@ -1,10 +1,7 @@
 #include "timer.h"
 
 
-/** @brief Get a time interval between now and given start time
- *  @param start The start time point
- *  @return a double represent time interval in second.
- */
+
 int get_time_diff(struct timeval* start) {
 	struct timeval now;
 	double t1 = start->tv_sec+(start->tv_usec/1000000.0);
